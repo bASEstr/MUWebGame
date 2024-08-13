2a44fe43831a57636ef21ccbd6aac82fd191a157
bASEstr <>
2024-08-12 21:31:59 -0300

    init server_01

---
+++ Server_S1/loginserver/start.bat
@@ -0,0 +1,4 @@
+@echo off
+set path=%cd%\jdk\bin
+title LoginServer
+java -jar -server -Xms2024M -Xmx2024M -Xmn700M -Xss1024K -Djava.util.Arrays.useLegacyMergeSort=true -XX:+DisableExplicitGC -XX:SurvivorRatio=1 -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSParallelRemarkEnabled -XX:+UseCMSCompactAtFullCollection -XX:CMSFullGCsBeforeCompaction=0 -XX:+CMSClassUnloadingEnabled -XX:LargePageSizeInBytes=128M -XX:+UseFastAccessorMethods -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=80 -XX:SoftRefLRUPolicyMSPerMB=0 -XX:+PrintClassHistogram -XX:+PrintGCDetails -XX:+PrintGCTimeStamps login.jar &
